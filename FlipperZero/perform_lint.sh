6480fe26e0a30af859b90db6f0a97365e4650098
Ilya Yachin <uin153974748@gmail.com>
2020-10-03 16:20:44 +1000

    resolving problems

--- FlipperZero/perform_lint.sh
+++ FlipperZero/perform_lint.sh
@@ -1,12 +1,12 @@
 # Lint with systemwide installation if present
-if which swiftlint >/dev/null; then
-  swiftlint
-  exit 0
-fi
+#if command -v swiftlint >/dev/null; then
+#  swiftlint
+#  exit 0
+#fi
 # Download a copy of swiftlint if it doesn't exist in project root
 if [ ! -f swiftlint ]; then
-  curl -o- -Ls https://github.com/realm/SwiftLint/releases/latest/download/portable_swiftlint.zip | tar xzf - swiftlint \
-  && chmod +x swiftlint || echo "warning: Failed to download SwiftLint"; exit 0
+  (curl -o- -Ls https://github.com/realm/SwiftLint/releases/latest/download/portable_swiftlint.zip | tar xzf - swiftlint \
+  && chmod +x swiftlint) || (echo "warning: Failed to download SwiftLint"; exit 0)
 fi
 # Lint with local swiftlint
 ./swiftlint
