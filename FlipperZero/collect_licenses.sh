fd957ebcaafd9f5858f0afa25c8d549c18cc1018
Ilya Yachin <uin153974748@gmail.com>
2021-04-05 11:44:45 +1000

    add licenses from Pods and Packages to Settings.bundle on build

---
+++ FlipperZero/collect_licenses.sh
@@ -0,0 +1,8 @@
+# Download a copy of LicensePlist if it doesn't exist.
+if [ ! -f license-plist ]; then
+    (curl -o- -Ls https://github.com/mono0926/LicensePlist/releases/latest/download/portable_licenseplist.zip | tar xzf - license-plist \
+    && chmod +x license-plist) || (echo "warning: Failed to download LicensePlist"; exit 0)
+fi
+echo $PRODUCT_NAME
+# (Re-)collect and (re-)generate Settings.bundle with 3rd pty. licenses.
+./license-plist --output-path Settings.bundle --add-version-numbers --prefix acknowledgements
