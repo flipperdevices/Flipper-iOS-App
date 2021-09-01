06d05e5ddd7b5361e28401b340cab86d90f69466
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-09-02 02:25:10 +0300

    Remove TabView

--- FlipperZero/UI/Sources/View/RootView.swift
+++ FlipperZero/UI/Sources/View/RootView.swift
@@ -2,20 +2,13 @@ import SwiftUI
 
 public struct RootView: View {
     let viewModel: RootViewModel
-    let homeTabTitle = "Home"
 
     public init(viewModel: RootViewModel) {
         self.viewModel = viewModel
     }
 
     public var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text(self.homeTabTitle)
-                }
-        }
+        HomeView()
     }
 }
 
