68539a1e1ec943525feff4073117af39cf5a2e78
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-09-06 17:58:06 +0300

    Extract DeviceInformation and Battery to typed services

--- FlipperZero/UI/Sources/View/DeviceInfoView.swift
+++ FlipperZero/UI/Sources/View/DeviceInfoView.swift
@@ -1,3 +1,4 @@
+import Core
 import SwiftUI
 
 struct DeviceInfoView: View {
@@ -12,15 +13,16 @@ struct DeviceInfoView: View {
             VStack {
                 Spacer()
                 Form {
+                    if let deviceInformation = device.deviceInformation {
+                        DeviceInformationService(deviceInformation)
+                    }
+                    if let battery = device.battery {
+                        BatteryService(battery)
+                    }
                     ForEach(device.services) { service in
                         Section(header: Text(service.name)) {
-                            List(service.characteristics) { characteristic in
-                                HStack {
-                                    Text("\(characteristic.name)")
-                                    Spacer()
-                                    Text("\(characteristic.value)")
-                                        .foregroundColor(.secondary)
-                                }
+                            ForEach(service.characteristics) { characteristic in
+                                CharacteristicSectionRow(characteristic)
                             }
                         }
                     }
@@ -31,6 +33,64 @@ struct DeviceInfoView: View {
             }
         } else {
             Text("No device connected")
+        }
+    }
+}
+
+struct DeviceInformationService: View {
+    let deviceInformation: Peripheral.Service.DeviceInformation
+
+    init(_ deviceInformation: Peripheral.Service.DeviceInformation) {
+        self.deviceInformation = deviceInformation
+    }
+
+    var body: some View {
+        Section(header: Text("Device Information")) {
+            CharacteristicSectionRow(deviceInformation.manufacturerName)
+            CharacteristicSectionRow(deviceInformation.modelNumber)
+            CharacteristicSectionRow(deviceInformation.firmwareRevision)
+            CharacteristicSectionRow(deviceInformation.softwareRevision)
+        }
+    }
+}
+
+struct BatteryService: View {
+    let battery: Peripheral.Service.Battery
+
+    init(_ battery: Peripheral.Service.Battery) {
+        self.battery = battery
+    }
+
+    var body: some View {
+        Section(header: Text("Battery")) {
+            CharacteristicSectionRow(battery.level)
+        }
+    }
+}
+
+struct CharacteristicSectionRow: View {
+    let characteristic: Peripheral.Service.Characteristic
+
+    init(_ characteristic: Peripheral.Service.Characteristic) {
+        self.characteristic = characteristic
+    }
+
+    var body: some View {
+        SectionRow(name: characteristic.name, value: characteristic.value)
+    }
+}
+
+struct SectionRow: View {
+    let name: String
+    let value: String
+
+    var body: some View {
+        HStack {
+            Text("\(name)")
+            Spacer()
+            Text("\(value)")
+                .multilineTextAlignment(.trailing)
+                .foregroundColor(.secondary)
         }
     }
 }
