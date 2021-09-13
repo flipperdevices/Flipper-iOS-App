6db7d9da787e3b9d7183b5471a8093060b8c5eb0
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-09-14 02:38:44 +0300

    Update ArchiveView

--- FlipperZero/UI/Sources/View/ArchiveView.swift
+++ FlipperZero/UI/Sources/View/ArchiveView.swift
@@ -3,24 +3,79 @@ import SwiftUI
 
 struct ArchiveView: View {
     @ObservedObject var viewModel: ArchiveViewModel
+    @Environment(\.colorScheme) var colorScheme
 
     init(viewModel: ArchiveViewModel) {
         self.viewModel = viewModel
     }
 
     var body: some View {
-        VStack(alignment: .center) {
-            List(viewModel.items) { item in
-                ArchiveListItemView(item: item)
-            }
+        VStack {
+            navbar
+            categories
+            items
 
-            Spacer()
-
-            RoundedButton("Read NFC Tag") {
-                viewModel.readNFCTag()
-            }
-            .padding(.bottom, 20)
         }
+    }
+
+    // FIXME: iOS15 beta bug, use navbar
+    var navbar: some View {
+        HStack {
+            Image(systemName: "ellipsis.circle")
+                .resizable()
+                .frame(width: 22, height: 22)
+                .scaledToFit()
+                .padding(.horizontal, 15)
+                .foregroundColor(Color.accentColor)
+            Spacer()
+            ZStack(alignment: .center) {
+                Image("CurrentDevice")
+                Text("Ak1r@")
+                    .bold()
+                    .padding(.bottom, 5)
+                    .foregroundColor(Color.primary)
+            }
+            Spacer()
+            Button(action: { viewModel.readNFCTag() }) {
+                Image(systemName: "plus.circle")
+                    .resizable()
+                    .frame(width: 22, height: 22)
+                    .scaledToFit()
+                    .padding(.horizontal, 15)
+                    .foregroundColor(Color.accentColor)
+            }
+        }
+        .frame(height: 44)
+    }
+
+    var categories: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(["Favorites", "RFID 125", "Sub-gHz", "NFC", "iButton", "iRda"], id: \.self) {
+                    Text($0)
+                        .padding(.horizontal, 7)
+                        .padding(.vertical, 12)
+                        .foregroundColor(Color.secondary)
+                }
+            }
+        }
+        .padding(.horizontal, 10)
+    }
+
+    var items: some View {
+        ScrollView {
+            Spacer(minLength: 12)
+            ForEach(demo) { item in
+                ArchiveListItemView(item: item)
+                    .background(colorScheme == .light ? Color.white : Color.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+        }
+        .navigationBarHidden(true)
+        .frame(maxWidth: .infinity)
+        .padding(.leading, 16)
+        .padding(.trailing, 15)
+        .background(Color.gray.opacity(0.1))
     }
 }
 
@@ -33,6 +88,9 @@ struct ArchiveListItemView: View {
                 .resizable()
                 .frame(width: 23, height: 23)
                 .scaledToFit()
+                .padding(.horizontal, 17)
+                .padding(.vertical, 22)
+                .background(item.color)
             VStack(spacing: 10) {
                 HStack {
                     Text(item.name)
@@ -40,6 +98,7 @@ struct ArchiveListItemView: View {
                     if item.isFavorite {
                         Spacer()
                         Image(systemName: "star.fill")
+                            .padding(.horizontal, 10)
                             .foregroundColor(.secondary)
                     }
                 }
@@ -48,10 +107,34 @@ struct ArchiveListItemView: View {
                     Text(item.description)
                     Spacer()
                     Text(item.origin)
-                    Image(systemName: "checkmark")
-                        .foregroundColor(.secondary)
+                    Image("cloud.checkmark")
+                        .padding(.horizontal, 10)
                 }
             }
+        }
+    }
+}
+
+extension ArchiveItem {
+    var icon: Image {
+        switch kind {
+        case .ibutton: return .init("ibutton")
+        case .nfc: return .init("nfc")
+        case .rfid: return .init("rfid")
+        case .subghz: return .init("subhz")
+        case .irda: return .init("irda")
+        }
+    }
+}
+
+extension ArchiveItem {
+    var color: Color {
+        switch kind {
+        case .ibutton: return .init(red: 0.0, green: 0.48, blue: 1.0)
+        case .nfc: return .init(red: 0.2, green: 0.78, blue: 0.64)
+        case .rfid: return .init(red: 0.35, green: 0.34, blue: 0.84)
+        case .subghz: return .init(red: 1.0, green: 0.61, blue: 0.2)
+        case .irda: return .init(red: 0.69, green: 0.32, blue: 0.87)
         }
     }
 }
@@ -61,3 +144,4 @@ struct ArchiveView_Previews: PreviewProvider {
         ArchiveView(viewModel: .init())
     }
 }
+
--- FlipperZero/UI/Sources/ViewModel/ArchiveViewModel.swift
+++ FlipperZero/UI/Sources/ViewModel/ArchiveViewModel.swift
@@ -31,13 +31,43 @@ class ArchiveViewModel: ObservableObject {
     }
 }
 
-extension ArchiveItem {
-    var icon: Image {
-        switch kind {
-        case .ibutton: return .init("ibutton")
-        case .nfc: return .init("nfc")
-        case .rfid: return .init("rfid")
-        case .subghz: return .init("subhz")
-        }
-    }
+var demo: [ArchiveItem] {
+    [
+        .init(
+            id: "Office_guest_pass",
+            name: "Office_guest_pass",
+            description: "ID: 031,33351",
+            isFavorite: true,
+            kind: .rfid,
+            wut: "EM-Marin"),
+        .init(
+            id: "Moms_bank_card",
+            name: "Moms_bank_card",
+            description: "ID: 031,33351",
+            isFavorite: true,
+            kind: .nfc,
+            wut: "Mifare"),
+        .init(
+            id: "Open_garage_door",
+            name: "Open_garage_door",
+            description: "868,86 MHz",
+            isFavorite: true,
+            kind: .subghz,
+            wut: "Doorhan"),
+        .init(
+            id: "Unknown_space_portal",
+            name: "Unknown_space_portal",
+            description: "ID: 03F4",
+            isFavorite: true,
+            kind: .ibutton,
+            wut: "Cyfral"),
+        .init(
+            id: "Edifier_speaker",
+            name: "Edifier_speaker",
+            description: "",
+            isFavorite: true,
+            kind: .irda,
+            wut: "")
+        
+    ]
 }
