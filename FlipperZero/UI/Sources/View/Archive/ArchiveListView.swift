d30818c92f71369a5835d2243bd673d0727c3bca
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-09-15 20:34:24 +0300

    Move AchiveItem extensions

--- FlipperZero/UI/Sources/View/Archive/ArchiveListView.swift
+++ FlipperZero/UI/Sources/View/Archive/ArchiveListView.swift
@@ -68,27 +68,3 @@ struct ArchiveListItemView: View {
         }
     }
 }
-
-extension ArchiveItem {
-    var icon: Image {
-        switch kind {
-        case .ibutton: return .init("ibutton")
-        case .nfc: return .init("nfc")
-        case .rfid: return .init("rfid")
-        case .subghz: return .init("subhz")
-        case .irda: return .init("irda")
-        }
-    }
-}
-
-extension ArchiveItem {
-    var color: Color {
-        switch kind {
-        case .ibutton: return .init(red: 0.0, green: 0.48, blue: 1.0)
-        case .nfc: return .init(red: 0.2, green: 0.78, blue: 0.64)
-        case .rfid: return .init(red: 0.35, green: 0.34, blue: 0.84)
-        case .subghz: return .init(red: 1.0, green: 0.61, blue: 0.2)
-        case .irda: return .init(red: 0.69, green: 0.32, blue: 0.87)
-        }
-    }
-}
