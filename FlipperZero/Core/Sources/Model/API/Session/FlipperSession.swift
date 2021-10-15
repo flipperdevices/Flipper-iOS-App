6dee2f1786e88b0f1291296a2eecdc7040a5032d
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-10-15 15:41:34 +0300

    Add command id

--- FlipperZero/Core/Sources/Model/API/Session/FlipperSession.swift
+++ FlipperZero/Core/Sources/Model/API/Session/FlipperSession.swift
@@ -10,6 +10,16 @@ class FlipperSession: Session {
     let sequencedRequest: SequencedRequest = .init()
     let chunkedRequest: ChunkedRequest = .init()
 
+    var requestId = 0 {
+        didSet {
+            let commandIdSize = MemoryLayout.size(ofValue: PB_Main().commandID)
+            let maxValue = Int(pow(2.0, Double(commandIdSize * 8)) - 1)
+            if requestId >= maxValue {
+                requestId = 0
+            }
+        }
+    }
+
     func sendRequest(
         _ request: Request,
         continuation: @escaping Session.Continuation,
@@ -19,7 +29,8 @@ class FlipperSession: Session {
         self.continuation = continuation
 
         let requests = sequencedRequest.split(request)
-        for request in requests {
+        for var request in requests {
+            request.commandID = .init(requestId)
             let chunks = chunkedRequest.split(request)
             for chunk in chunks {
                 assert(!chunk.isEmpty)
@@ -35,11 +46,14 @@ class FlipperSession: Session {
             guard let nextResponse = try chunkedResponse.feed(data) else {
                 return
             }
+            guard nextResponse.commandID == requestId else {
+                print("invalid id")
+                return
+            }
             // complete PB_Main can be split into multiple messages
             guard let response = try sequencedResponse.feed(nextResponse) else {
                 return
             }
-            // TODO: Compare message id
             if case .error(let error) = response {
                 print(error)
             }
@@ -49,6 +63,7 @@ class FlipperSession: Session {
             }
             self.request = nil
             self.continuation = nil
+            requestId += 1
             continuation(response)
         } catch {
             print(error)
