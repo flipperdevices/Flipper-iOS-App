bf3fe9b4356faf222cfe7f4d2533696cfcd98348
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-10-25 23:45:57 +0300

    Remove demo items

--- FlipperZero/Core/Sources/Model/Archive/Archive.swift
+++ FlipperZero/Core/Sources/Model/Archive/Archive.swift
@@ -17,9 +17,6 @@ public class Archive: ObservableObject {
 
     private init() {
         items = storage.items
-        if items.isEmpty {
-            items = demo
-        }
     }
 
     public func append(_ item: ArchiveItem) {
@@ -41,44 +38,4 @@ public class Archive: ObservableObject {
             completion()
         }
     }
-}
-
-var demo: [ArchiveItem] {
-    [
-        .init(
-            id: "Office_guest_pass",
-            name: "Office_guest_pass",
-            description: "ID: 031,33351",
-            isFavorite: true,
-            kind: .rfid,
-            origin: "EM-Marin"),
-        .init(
-            id: "Moms_bank_card",
-            name: "Moms_bank_card",
-            description: "ID: 031,33351",
-            isFavorite: true,
-            kind: .nfc,
-            origin: "Mifare"),
-        .init(
-            id: "Open_garage_door",
-            name: "Open_garage_door",
-            description: "868,86 MHz",
-            isFavorite: true,
-            kind: .subghz,
-            origin: "Doorhan"),
-        .init(
-            id: "Unknown_space_portal",
-            name: "Unknown_space_portal",
-            description: "ID: 03F4",
-            isFavorite: true,
-            kind: .ibutton,
-            origin: "Cyfral"),
-        .init(
-            id: "Edifier_speaker",
-            name: "Edifier_speaker",
-            description: "",
-            isFavorite: true,
-            kind: .irda,
-            origin: "")
-    ]
 }
