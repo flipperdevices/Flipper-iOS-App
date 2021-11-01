6dcf78371e60e96f64ce2017307e0a90e6e5ba95
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-11-01 17:55:18 +0300

    Delete migration from UserDefaults

--- FlipperZero/Core/Sources/Service/Platform/JSONDeviceStorage.swift
+++ FlipperZero/Core/Sources/Service/Platform/JSONDeviceStorage.swift
@@ -10,19 +10,8 @@ class JSONDeviceStorage: DeviceStorage {
         storage = .init(for: Peripheral.self, filename: "paired_device")
     }
 
-    func migrate() -> Peripheral? {
-        guard let uuid = UserDefaultsStorage().lastConnectedDevice else {
-            return nil
-        }
-        UserDefaultsStorage().lastConnectedDevice = nil
-        return Peripheral(id: uuid, name: "Unknown")
-    }
-
     func read() -> Peripheral? {
-        if let peripheral = migrate() {
-            return peripheral
-        }
-        return storage.read()
+        storage.read()
     }
 
     func write(_ peripheral: Peripheral?) {
--- FlipperZero/Core/Sources/Service/Platform/UserDefaultsStorage.swift
+++ FlipperZero/Core/Sources/Service/Platform/UserDefaultsStorage.swift
@@ -2,27 +2,4 @@ import Foundation
 
 class UserDefaultsStorage {
     var storage: UserDefaults { .standard }
-
-    let lastDeviceKey: String = "lastConnectedDeviceUUIDString"
-
-    var lastConnectedDevice: UUID? {
-        get {
-            guard
-                let value = storage.value(forKey: lastDeviceKey) as? String,
-                let uuid = UUID(uuidString: value)
-            else {
-                return nil
-            }
-            return uuid
-        }
-
-        set {
-            switch newValue {
-            case let .some(uuid):
-                storage.set(uuid.uuidString, forKey: lastDeviceKey)
-            case .none:
-                storage.removeObject(forKey: lastDeviceKey)
-            }
-        }
-    }
 }
