e28903b8b863a11327609e1c6dda23f65a6e4f30
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-11-23 19:21:09 +0300

    Use mtu to chunk request

--- FlipperZero/Core/Sources/Model/API/Session/ChunkedRequest.swift
+++ FlipperZero/Core/Sources/Model/API/Session/ChunkedRequest.swift
@@ -1,8 +1,34 @@
+import struct Foundation.Data
+
 class ChunkedRequest {
-    func split(_ main: PB_Main) -> [[UInt8]] {
-        // shouldn't throw
-        // swiftlint:disable force_try
-        let bytes = try! main.serialize()
-        return bytes.chunk(maxCount: Limits.maxBLEPacket)
+    private var queue: Data = .init()
+    private var packetSize: Int = 38
+    private var freeSpace = 0
+
+    weak var delegate: PeripheralOutputDelegate?
+
+    func feed(_ requests: [PB_Main]) {
+        for request in requests {
+            // shouldn't throw
+            // swiftlint:disable force_try
+            queue.append(contentsOf: try! request.serialize())
+        }
+        processDataQueue()
+    }
+
+    func didReceiveFlowControl(freeSpace: Int, packetSize: Int) {
+        self.freeSpace = freeSpace
+        self.packetSize = packetSize
+        processDataQueue()
+    }
+
+    private func processDataQueue() {
+        while freeSpace > 0 && !queue.isEmpty {
+            let size = min(freeSpace, packetSize)
+            let data = queue.prefix(size)
+            queue.removeFirst(data.count)
+            freeSpace -= data.count
+            delegate?.send(data)
+        }
     }
 }
