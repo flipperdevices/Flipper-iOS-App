d280d0571e47a2a080d5de561d7d9e227a034265
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-11-23 16:45:05 +0300

    Increase ble packet length

--- FlipperZero/Core/Sources/Model/API/Limits.swift
+++ FlipperZero/Core/Sources/Model/API/Limits.swift
@@ -2,5 +2,5 @@ enum Limits {
     // NOTE: not used yet
     // static var maxPbPacket: Int { 1536 }
     static var maxPBStorageFileData: Int { 512 }
-    static var maxBLEPacket: Int { 248 }
+    static var maxBLEPacket: Int { 486 }
 }
