aab904a1792b126c24fb130c6793f2f643b3f7e6
Tony Freeman <tonyfreeman@users.noreply.github.com>
2021-12-03 03:06:41 +0300

    Fix dependencies

--- FlipperZero/Packages/Core/Sources/Dependencies.swift
+++ FlipperZero/Packages/Core/Sources/Dependencies.swift
@@ -2,10 +2,11 @@ import Inject
 
 public func registerDependencies() {
     let container = Container.shared
-    container.register(FlipperCentral.init, as: BluetoothCentral.self, isSingleton: true)
-    container.register(FlipperCentral.init, as: BluetoothConnector.self, isSingleton: true)
+    let central = FlipperCentral()
+    container.register(instance: central, as: BluetoothCentral.self)
+    container.register(instance: central, as: BluetoothConnector.self)
     container.register(PairedFlipper.init, as: PairedDevice.self, isSingleton: true)
-    container.register(IOSNFCService.init, as: NFCService.self, isSingleton: false)
     container.register(JSONDeviceStorage.init, as: DeviceStorage.self, isSingleton: true)
     container.register(JSONArchiveStorage.init, as: ArchiveStorage.self, isSingleton: true)
+    container.register(IOSNFCService.init, as: NFCService.self)
 }
