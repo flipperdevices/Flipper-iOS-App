bac25b4004c9b6b1df9dbadb345742eef153314d
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-25 22:40:06 +0300

    Move Manifest

--- Flipper/Packages/Core/Sources/Archive/Manifest/Manifest.swift
+++ Flipper/Packages/Core/Sources/Archive/Manifest/Manifest.swift
@@ -2,11 +2,11 @@ public struct Manifest: Codable {
     var items: [Item]
 
     public struct Item: Equatable, Codable {
-        public let path: Path
+        public let id: ArchiveItem.ID
         public let hash: Hash
     }
 
-    subscript(_ path: Path) -> Item? {
-        return items.first { $0.path == path }
+    subscript(_ id: ArchiveItem.ID) -> Item? {
+        return items.first { $0.id == id }
     }
 }
