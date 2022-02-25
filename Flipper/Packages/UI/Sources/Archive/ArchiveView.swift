afca040986a902dbb29635e93b66e9ec5830f6e6
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-02-25 03:05:13 +0300

    Remove ArchiveView components

--- Flipper/Packages/UI/Sources/Archive/ArchiveView.swift
+++ Flipper/Packages/UI/Sources/Archive/ArchiveView.swift
@@ -6,86 +6,22 @@ struct ArchiveView: View {
     @StateObject var viewModel: ArchiveViewModel
 
     var body: some View {
-        VStack(spacing: 0) {
-            ArchiveHeaderView(
-                viewModel: viewModel)
-            ArchiveCategoriesView(
-                categories: viewModel.categories,
-                selectedIndex: $viewModel.selectedCategoryIndex)
-            Divider()
-            CarouselView(
-                spacing: 0,
-                index: $viewModel.selectedCategoryIndex,
-                items: viewModel.itemGroups
-            ) { group in
-                ArchiveListView(
-                    status: viewModel.status,
-                    items: group.items,
-                    hasFavorites: group == viewModel.itemGroups.first,
-                    isSelectItemsMode: $viewModel.isSelectItemsMode,
-                    selectedItems: $viewModel.selectedItems,
-                    onAction: onAction)
+        NavigationView {
+            VStack {
+                Text("Content")
             }
-
-            if viewModel.isSelectItemsMode {
-                tabViewOverlay
-            }
-        }
-        .actionSheet(isPresented: $viewModel.isDeletePresented) {
-            .init(title: Text("You can't undo this action"), buttons: [
-                .destructive(Text("Delete")) {
-                    viewModel.deleteSelectedItems()
-                },
-                .cancel()
-            ])
-        }
-    }
-
-    func onAction(_ action: ArchiveListView.Action) {
-        switch action {
-        case .itemSelected(let item): onItemSelected(item: item)
-        case .synchronize: viewModel.synchronize()
-        }
-    }
-
-    func onItemSelected(item: ArchiveItem) {
-        if viewModel.isSelectItemsMode {
-            viewModel.selectItem(item)
-        } else {
-            viewModel.editingItem = .init(item)
-            viewModel.sheetManager.present {
-                CardSheetView(viewModel: viewModel)
-            }
-        }
-    }
-
-    var tabViewOverlay: some View {
-        VStack {
-            HStack(alignment: .center) {
-                Button {
-                    viewModel.shareSelectedItems()
-                } label: {
-                    Image(systemName: "square.and.arrow.up")
-                        .font(.system(size: 22))
+            .edgesIgnoringSafeArea(.top)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Text("Archive")
+                        .font(.system(size: 20, weight: .bold))
                 }
-
-                Spacer()
-                Text("Chosen \(viewModel.selectedItems.count) objects")
-                    .font(.system(size: 17, weight: .semibold))
-                Spacer()
-
-                Button {
-                    viewModel.isDeletePresented = true
-                } label: {
-                    Image(systemName: "trash")
-                        .font(.system(size: 22))
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "magnifyingglass")
+                        .font(.system(size: 18, weight: .bold))
                 }
             }
-            .padding(.top, 12)
-            .padding(.bottom, bottomSafeArea)
-            .padding(.horizontal, 22)
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .frame(height: tabViewHeight + bottomSafeArea + 8, alignment: .top)
-        .background(systemBackground)
     }
 }
