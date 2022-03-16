1f49ae195e0853c618b0b23de0d228fee39a7861
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-03-16 23:11:53 +0300

    Filter invalid characters

--- Flipper/Packages/UI/Sources/Archive/CardView/Name/CardNameEditView.swift
+++ Flipper/Packages/UI/Sources/Archive/CardView/Name/CardNameEditView.swift
@@ -1,10 +1,14 @@
 import Core
 import SwiftUI
+import Combine
 
 extension CardView {
     struct CardNameEditView: View {
         @Binding var item: ArchiveItem
         @Binding var focusedField: String
+
+        let nameLimit = 22
+        let noteLimit = 120
 
         var body: some View {
             VStack(alignment: .leading, spacing: 18) {
@@ -15,6 +19,11 @@ extension CardView {
                     focusedField: $focusedField
                 )
                 .font(.system(size: 14, weight: .medium))
+                .onReceive(Just(item.name.value)) { newValue in
+                    let filtered = newValue.filtered().prefix(nameLimit)
+                    guard filtered != newValue else { return }
+                    item.name.value = String(filtered)
+                }
 
                 UTextField(
                     title: "Note:",
@@ -23,7 +32,24 @@ extension CardView {
                     focusedField: $focusedField
                 )
                 .font(.system(size: 14, weight: .medium))
+                .onReceive(Just(item.name.value)) { newValue in
+                    let filtered = newValue.prefix(noteLimit)
+                    guard filtered != newValue else { return }
+                    item.name.value = String(filtered)
+                }
             }
         }
+    }
+}
+
+private extension StringProtocol {
+    var allowedCharacters: String {
+        #"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"# +
+        #"!#\$%&'()-@^_`{}~"#
+    }
+
+    func filtered() -> String {
+        self.replacingOccurrences(of: " ", with: "_")
+            .filter { allowedCharacters.contains($0) }
     }
 }
