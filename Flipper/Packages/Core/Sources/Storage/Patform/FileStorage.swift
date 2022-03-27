0f3c3e13126d7b2369d4d54a72c101e95ad42df0
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-03-28 00:38:42 +0300

    Add FileStorage

---
+++ Flipper/Packages/Core/Sources/Storage/Patform/FileStorage.swift
@@ -0,0 +1,60 @@
+import Bluetooth
+import Foundation
+
+class FileStorage {
+    var baseURL: URL {
+        let paths = FileManager.default.urls(
+            for: .applicationSupportDirectory,
+            in: .userDomainMask)
+        return paths[0]
+    }
+
+    init() {}
+
+    func makeDirectory(for path: Path) throws {
+        let subdirectory = path.removingLastComponent
+        let directory = baseURL.appendingPathComponent(subdirectory.string)
+        if !FileManager.default.fileExists(atPath: directory.path) {
+            try FileManager.default.createDirectory(
+                at: directory,
+                withIntermediateDirectories: true)
+        }
+    }
+
+    private func makeURL(for path: Path) -> URL {
+        baseURL.appendingPathComponent(path.string)
+    }
+
+    func read(_ path: Path) throws -> String {
+        let url = makeURL(for: path)
+        return try .init(contentsOf: url)
+    }
+
+    func write(content: String, to path: Path) throws {
+        try makeDirectory(for: path)
+        let url = makeURL(for: path)
+        try content.write(to: url, atomically: true, encoding: .utf8)
+    }
+
+    func delete(_ path: Path) throws {
+        let url = makeURL(for: path)
+        try FileManager.default.removeItem(at: url)
+    }
+
+    func reset() throws {
+        let contents = try FileManager
+            .default
+            .contentsOfDirectory(atPath: baseURL.path)
+
+        for path in contents {
+            let url = baseURL.appendingPathComponent(path)
+            try FileManager.default.removeItem(at: url)
+        }
+    }
+}
+
+extension URL {
+    var isExists: Bool {
+        FileManager.default.fileExists(atPath: path)
+    }
+}
