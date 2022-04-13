9cee06a03e3bf562ab64b792898bd4dd316a36d6
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-04-13 04:30:31 +0300

    Update Outdated alert

--- Flipper/Packages/UI/Sources/Device/Components/UnsupportedDeviceSection.swift
+++ Flipper/Packages/UI/Sources/Device/Components/UnsupportedDeviceSection.swift
@@ -10,19 +10,19 @@ struct UnsupportedDeviceSection: View {
                     Spacer()
                 }
 
-                Image("Update")
-                    .padding(.top, 18)
+                Image("Outdated")
+                    .padding(.top, 9)
 
                 Text("Outdated firmware version")
                     .font(.system(size: 14, weight: .medium))
                     .multilineTextAlignment(.center)
-                    .padding(.top, 12)
+                    .padding(.top, 4)
 
                 Text("Firmware version on your Flipper is not supported. Please update it via PC.")
                     .font(.system(size: 14, weight: .medium))
                     .multilineTextAlignment(.center)
                     .foregroundColor(.black40)
-                    .padding(.top, 8)
+                    .padding(.top, 4)
 
                 Button {
                     UIApplication.shared.open(.helpToInstallFirmware)
@@ -32,7 +32,7 @@ struct UnsupportedDeviceSection: View {
                 }
                 .font(.system(size: 14, weight: .medium))
                 .foregroundColor(.a2)
-                .padding(.top, 18)
+                .padding(.top, 8)
             }
             .padding(12)
         }
