91f4fe8782eecc21ffa3e51d9e6fab00ec675b5e
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-06-02 13:16:14 +0300

    Add customAlert modifier

---
+++ Flipper/Packages/UI/Sources/Common/Alerts/CusomAlert.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+extension View {
+    func customAlert<Content: View>(
+        isPresented: Binding<Bool>,
+        @ViewBuilder content: @escaping () -> Content
+    ) -> some View {
+        popup(isPresented: isPresented) {
+            VStack(spacing: 0) {
+                HStack {
+                    Spacer()
+                    Button {
+                        withoutAnimation {
+                            isPresented.wrappedValue = false
+                        }
+                    } label: {
+                        Image(systemName: "xmark")
+                    }
+                    .font(.system(size: 20, weight: .medium))
+                    .foregroundColor(.primary)
+                }
+                .padding(.top, 19)
+                .padding(.trailing, 19)
+
+                content()
+                    .padding(.horizontal, 12)
+                    .padding(.bottom, 12)
+            }
+            .frame(width: 292)
+            .background(RoundedRectangle(cornerRadius: 18)
+                .fill(Color.secondaryGroupedBackground)
+            )
+        }
+    }
+}
