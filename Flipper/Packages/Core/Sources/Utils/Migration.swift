d4cf4585c894711baeb438265f9f3bcf95f21d75
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-10-25 00:11:21 +0300

    Reset app on downgrade

--- Flipper/Packages/Core/Sources/Utils/Migration.swift
+++ Flipper/Packages/Core/Sources/Utils/Migration.swift
@@ -1,17 +1,38 @@
 import Foundation
 
 public func migration() {
+    // check if version is changed
     guard UserDefaults.lastVersion != Bundle.fullVersion else {
         return
     }
 
-    if UserDefaults.lastVersion == "()" {
-        UserDefaultsStorage.shared.reset()
-        try? FileStorage().reset()
+    defer {
+        UserDefaults.lastRelease = Bundle.releaseVersion
+        UserDefaults.lastBuild = Bundle.buildVersion
     }
 
-    UserDefaults.lastRelease = Bundle.releaseVersion
-    UserDefaults.lastBuild = Bundle.buildVersion
+    // guard pre-migration apps
+    guard UserDefaults.lastVersion != "()" else {
+        resetStorage()
+        return
+    }
+    guard // ignore developer build
+        let previousBuild = Int(UserDefaults.lastBuild), previousBuild > 0,
+        let currentBuild = Int(Bundle.buildVersion), currentBuild > 0
+    else {
+        return
+    }
+
+    // reset storage on downgrade
+    guard currentBuild > previousBuild else {
+        resetStorage()
+        return
+    }
+}
+
+private func resetStorage() {
+    UserDefaultsStorage.shared.reset()
+    try? FileStorage().reset()
 }
 
 extension UserDefaults {
