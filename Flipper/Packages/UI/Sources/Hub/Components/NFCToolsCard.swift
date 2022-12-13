cc556d114bba38e731df453d1b0c1f30ad9803b1
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-12-14 00:54:37 +0300

    Update Hub card layout

--- Flipper/Packages/UI/Sources/Hub/Components/NFCToolsCard.swift
+++ Flipper/Packages/UI/Sources/Hub/Components/NFCToolsCard.swift
@@ -4,24 +4,20 @@ struct NFCToolsCard: View {
     let hasNotification: Bool
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 14) {
-            Image("nfc")
-                .resizable()
-                .renderingMode(.template)
-                .frame(width: 30, height: 30)
-                .foregroundColor(.primary)
-
-            VStack(alignment: .leading, spacing: 4) {
-                Text("NFC Tools")
-                    .font(.system(size: 16, weight: .bold))
-                    .foregroundColor(.primary)
+        HubCard {
+            VStack(alignment: .leading, spacing: 12) {
                 HStack {
-                    Text(
-                        "Calculate MIFARE Classic card keys using Flipper Zero"
-                    )
-                    .font(.system(size: 12, weight: .medium))
-                    .multilineTextAlignment(.leading)
-                    .foregroundColor(.black30)
+                    HStack(spacing: 12) {
+                        Image("nfc")
+                            .resizable()
+                            .renderingMode(.template)
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.primary)
+
+                        Text("NFC Tools")
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundColor(.primary)
+                    }
 
                     Spacer(minLength: 8)
 
@@ -36,11 +32,12 @@ struct NFCToolsCard: View {
                             .frame(width: 14, height: 14)
                     }
                 }
+
+                Text("Calculate MIFARE Classic card keys using Flipper Zero")
+                    .font(.system(size: 14, weight: .medium))
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.black30)
             }
         }
-        .padding([.bottom, .leading, .top], 12)
-        .padding(.trailing, 8)
-        .background(Color.groupedBackground)
-        .cornerRadius(10)
     }
 }
