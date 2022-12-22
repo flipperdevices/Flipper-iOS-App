f6b05f7ddd74a1b7dc7ed83c56d482b780166988
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-12-22 16:12:18 +0300

    [Import] Replace loading spinner with card placeholer

--- Flipper/Packages/UI/Sources/Archive/Import/ImportView.swift
+++ Flipper/Packages/UI/Sources/Archive/Import/ImportView.swift
@@ -20,11 +20,17 @@ struct ImportView: View {
 
             switch viewModel.state {
             case .loading:
-                VStack {
-                    Spacer()
-                    Animation("Loading")
-                        .frame(width: 48, height: 48)
-                    Spacer()
+                ScrollView {
+                    VStack(spacing: 18) {
+                        CardPlaceholder()
+
+                        AnimatedPlaceholder()
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 41)
+                            .cornerRadius(30)
+                    }
+                    .padding(.top, 6)
+                    .padding(.horizontal, 24)
                 }
             case .error(.noInternet):
                 VStack {
@@ -76,6 +82,47 @@ struct ImportView: View {
         }
         .background(Color.background)
         .edgesIgnoringSafeArea(.bottom)
+    }
+
+    struct CardPlaceholder: View {
+        @Environment(\.colorScheme) var colorScheme
+
+        var dividerColor: Color {
+            colorScheme == .dark ? .black60 : .black4
+        }
+
+        var body: some View {
+            VStack(alignment: .leading, spacing: 18) {
+                AnimatedPlaceholder()
+                    .frame(width: 114, height: 44)
+                    .cornerRadius(18, corners: [.bottomRight])
+                    .offset(x: -4, y: -4)
+
+                VStack(alignment: .leading, spacing: 14) {
+                    AnimatedPlaceholder()
+                        .frame(width: 128, height: 16)
+                    AnimatedPlaceholder()
+                        .frame(width: 96, height: 12)
+                }
+                .padding(.horizontal, 12)
+
+                Divider()
+                    .frame(height: 1)
+                    .background(dividerColor)
+
+                VStack(alignment: .leading, spacing: 14) {
+                    AnimatedPlaceholder()
+                        .frame(width: 64, height: 12)
+                    AnimatedPlaceholder()
+                        .frame(width: 64, height: 12)
+                }
+                .padding(.horizontal, 12)
+                .padding(.bottom, 18)
+            }
+            .background(Color.groupedBackground)
+            .cornerRadius(16)
+            .shadow(color: .shadow, radius: 16, x: 0, y: 4)
+        }
     }
 
     struct NoInternetError: View {
