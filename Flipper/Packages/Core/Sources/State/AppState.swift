c36ddb82c9aedf2915328820bb3b1f62a4bc1be2
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-12-31 00:44:23 +0300

    Move appstate props

--- Flipper/Packages/Core/Sources/State/AppState.swift
+++ Flipper/Packages/Core/Sources/State/AppState.swift
@@ -9,6 +9,11 @@ import Logging
 public class AppState: ObservableObject {
     private let logger = Logger(label: "app-state")
 
+    @Published public var firstLaunch: FirstLaunch = .shared
+
+    @Published public var flipper: Flipper?
+    @Published public var status: DeviceStatus = .noDevice
+
     @Published public var update: UpdateModel = .init()
     @Published public var updateAvailable: VersionUpdateModel = .init()
 
@@ -16,11 +21,6 @@ public class AppState: ObservableObject {
     @Published public var readerAttack: ReaderAttackModel = .init()
 
     @Published public var widget: WidgetModel = .init()
-
-    @Published public var firstLaunch: FirstLaunch = .shared
-
-    @Published public var flipper: Flipper?
-    @Published public var status: DeviceStatus = .noDevice
 
     @Published public var hasMFLog = false
     public let imported = SafeSubject<ArchiveItem>()
