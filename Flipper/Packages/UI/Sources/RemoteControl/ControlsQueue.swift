8e76d3d6ec47cb0badf8249e86af45b0b5cf4006
Tony Freeman <tonyfreeman@users.noreply.github.com>
2023-04-07 19:25:58 +0300

    [RemoteControl] Update queue images

--- Flipper/Packages/UI/Sources/RemoteControl/ControlsQueue.swift
+++ Flipper/Packages/UI/Sources/RemoteControl/ControlsQueue.swift
@@ -10,16 +10,49 @@ struct ControlsQueue: View {
 
     var body: some View {
         ScrollView(.horizontal) {
-            HStack(spacing: 0) {
+            HStack(spacing: 8) {
                 ForEach(controlsQueue, id: \.0) { item in
-                    ControlButtonImage(item.1)
-                        .scaleEffect(0.7)
-                        .opacity(0.7)
+                    KeyImage(item.1)
                 }
-                Spacer()
             }
-            .frame(height: 40)
+            .frame(height: 18)
         }
         .disabled(true)
+    }
+
+    struct KeyImage: View {
+        let inputKey: InputKey
+
+        var rotation: Double {
+            switch inputKey {
+            case .up: return 0
+            case .left: return -90
+            case .right: return 90
+            case .down: return 180
+            default: return 0
+            }
+        }
+
+        var image: String {
+            switch inputKey {
+            case .up: return "QueueUp"
+            case .down: return "QueueUp"
+            case .left: return "QueueUp"
+            case .right: return "QueueUp"
+            case .enter: return "QueueEnter"
+            case .back: return "QueueBack"
+            }
+        }
+
+        init(_ inputKey: InputKey) {
+            self.inputKey = inputKey
+        }
+
+        var body: some View {
+            Image(image)
+                .resizable()
+                .scaledToFit()
+                .rotationEffect(.degrees(rotation))
+        }
     }
 }
