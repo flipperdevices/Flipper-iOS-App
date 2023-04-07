10a95e786e2df75031a67cec99e26a5af703db61
Tony Freeman <tonyfreeman@users.noreply.github.com>
2023-04-07 19:30:07 +0300

    [RemoteControl] Fix device frame padding

--- Flipper/Packages/UI/Sources/RemoteControl/DeviceScreen.swift
+++ Flipper/Packages/UI/Sources/RemoteControl/DeviceScreen.swift
@@ -13,11 +13,13 @@ struct DeviceScreen: View {
             .resizable()
             .scaledToFit()
             .overlay(
-                Image(uiImage: uiImage)
-                    .resizable()
-                    .interpolation(.none)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(14)
+                GeometryReader { proxy in
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .interpolation(.none)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(proxy.size.width * 0.04)
+                }
             )
     }
 }
