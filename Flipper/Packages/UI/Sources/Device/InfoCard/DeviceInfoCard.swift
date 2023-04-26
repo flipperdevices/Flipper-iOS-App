b137bf5c52e27bf07145e7d80e4ff50a1a42b29a
Tony Freeman <tonyfreeman@users.noreply.github.com>
2023-04-26 14:38:34 +0300

    Follow IEC 80000-13 standard

--- Flipper/Packages/UI/Sources/Device/InfoCard/DeviceInfoCard.swift
+++ Flipper/Packages/UI/Sources/Device/InfoCard/DeviceInfoCard.swift
@@ -177,8 +177,22 @@ extension StorageSpace: CustomStringConvertible {
 }
 
 fileprivate extension Int {
+    var bytes: Int {
+        1024
+    }
+
+    var units: [String] {
+        ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
+    }
+
     var hr: String {
-        let formatter = ByteCountFormatter()
-        return formatter.string(fromByteCount: Int64(self))
+        guard self >= bytes else { return "\(self) B" }
+        let exp = Int(log2(Double(self)) / log2(Double(bytes)))
+        let unit = units[exp - 1]
+        let number = Double(self) / pow(Double(bytes), Double(exp))
+        return (exp <= 1 || number >= 100)
+            ? String(format: "%.0f %@", number, unit)
+            : String(format: "%.1f %@", number, unit)
+                .replacingOccurrences(of: ".0", with: "")
     }
 }
