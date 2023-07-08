01db4dcfdd432d8b99e9f5be83ac2e89e63985f1
Tony Freeman <tonyfreeman@users.noreply.github.com>
2023-07-08 15:46:49 +0300

    Autofocus SearchField

--- Flipper/Packages/UI/Sources/Archive/Search/Components/SearchField.swift
+++ Flipper/Packages/UI/Sources/Archive/Search/Components/SearchField.swift
@@ -17,7 +17,7 @@ struct SearchField: View {
                     .opacity(predicate.isEmpty ? 1 : 0)
                     .foregroundColor(.primary.opacity(0.7))
 
-                TextField("", text: _predicate)
+                FocusedTextField("", text: $predicate)
                     .lineLimit(1)
                     .submitLabel(.done)
                     .font(.system(size: 17))
@@ -29,5 +29,55 @@ struct SearchField: View {
         }
         .background(Color(red: 0.46, green: 0.46, blue: 0.5, opacity: 0.12))
         .cornerRadius(10)
+    }
+}
+
+fileprivate struct FocusedTextField: UIViewRepresentable {
+    @Binding var text: String
+
+    init(_ titleKey: LocalizedStringKey, text: Binding<String>) {
+        _text = text
+    }
+
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField()
+        textField.delegate = context.coordinator
+        textField.returnKeyType = .done
+        textField.becomeFirstResponder()
+        return textField
+    }
+
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = text
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: FocusedTextField
+
+        init(_ textField: FocusedTextField) {
+            self.parent = textField
+        }
+
+        func textField(
+            _ textField: UITextField,
+            shouldChangeCharactersIn range: NSRange,
+            replacementString string: String
+        ) -> Bool {
+            if let currentValue = textField.text as NSString? {
+                let proposedValue = currentValue
+                    .replacingCharacters(in: range, with: string)
+                self.parent.text = proposedValue
+            }
+            return true
+        }
+
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+            return true
+        }
     }
 }
