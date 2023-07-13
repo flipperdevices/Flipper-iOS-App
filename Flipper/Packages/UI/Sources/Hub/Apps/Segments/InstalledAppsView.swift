60552c3ec9ee2bf33906b3dfbae721a8a3cd03ed
Tony Freeman <tonyfreeman@users.noreply.github.com>
2023-07-13 16:55:25 +0300

    [Catalog] Add more placeholders

--- Flipper/Packages/UI/Sources/Hub/Apps/Segments/InstalledAppsView.swift
+++ Flipper/Packages/UI/Sources/Hub/Apps/Segments/InstalledAppsView.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct InstalledAppsView: View {
     @EnvironmentObject var model: Applications
 
+    @State var isBusy = false
     @State var applications: [Applications.ApplicationInfo] = []
 
     var outdatedApplications: [Applications.ApplicationInfo] {
@@ -11,25 +12,35 @@ struct InstalledAppsView: View {
     }
 
     var body: some View {
-        ScrollView {
-            if !applications.isEmpty {
-                VStack(spacing: 18) {
-                    if model.outdatedCount > 0 {
-                        UpdateAllAppButton {
-                            for application in outdatedApplications {
-                                model.update(application.id)
-                            }
-                        }
-                        .padding(.horizontal, 14)
-                    }
-
-                    AppList(
-                        applications: applications,
-                        isInstalled: true)
+        Group {
+            if !isBusy && applications.isEmpty {
+                if model.deviceInfo == nil {
+                    NotConnected()
+                } else {
+                    NoApps()
                 }
-                .padding(.vertical, 14)
             } else {
-                InstalledAppsPreview()
+                ScrollView {
+                    if !applications.isEmpty {
+                        VStack(spacing: 18) {
+                            if model.outdatedCount > 0 {
+                                UpdateAllAppButton {
+                                    for application in outdatedApplications {
+                                        model.update(application.id)
+                                    }
+                                }
+                                .padding(.horizontal, 14)
+                            }
+
+                            AppList(
+                                applications: applications,
+                                isInstalled: true)
+                        }
+                        .padding(.vertical, 14)
+                    } else {
+                        InstalledAppsPreview()
+                    }
+                }
             }
         }
         .onReceive(model.$manifests) { manifest in
@@ -53,12 +64,13 @@ struct InstalledAppsView: View {
             return
         }
         do {
+            isBusy = true
             applications = try await model.loadInstalled()
+            isBusy = false
         } catch {
             applications = []
         }
     }
-
 
     struct InstalledAppsPreview: View {
         var body: some View {
@@ -70,6 +82,34 @@ struct InstalledAppsView: View {
                 AppRowPreview(isInstalled: true)
             }
             .padding(.vertical, 14)
+        }
+    }
+
+    struct NoApps: View {
+        var body: some View {
+            Text("No apps installed on your Flipper yet")
+                .font(.system(size: 14, weight: .medium))
+                .multilineTextAlignment(.center)
+                .foregroundColor(.black40)
+        }
+    }
+
+    struct NotConnected: View {
+        var body: some View {
+            VStack(spacing: 4) {
+                Image("AppAlertNotConnected")
+
+                VStack(spacing: 4) {
+                    Text("Flipper is Not Connected")
+                        .font(.system(size: 14, weight: .bold))
+
+                    Text("Connect your Flipper Zero to install this app")
+                        .font(.system(size: 14, weight: .medium))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.black40)
+                }
+            }
+            .padding(.horizontal, 24)
         }
     }
 }
