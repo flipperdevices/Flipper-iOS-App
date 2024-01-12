91a841ee42ded4cc8c2767b4151f43e6e2d1fbb4
Tony Freeman <tonyfreeman@users.noreply.github.com>
2024-01-12 09:23:12 +0300

    Revert "Remove custom FocusedTextField"
    This reverts commit 49436832e386fa1860b893aa3652012072a3c232.

--- Flipper/Packages/UI/Sources/Archive/Search/Components/SearchField.swift
+++ Flipper/Packages/UI/Sources/Archive/Search/Components/SearchField.swift
@@ -4,8 +4,6 @@ struct SearchField: View {
     let placeholder: String
     @Binding var predicate: String
 
-    @FocusState var isFocused: Bool
-
     var body: some View {
         HStack(spacing: 6) {
             Image(systemName: "magnifyingglass")
@@ -19,21 +17,68 @@ struct SearchField: View {
                     .opacity(predicate.isEmpty ? 1 : 0)
                     .foregroundColor(.primary.opacity(0.7))
 
-                TextField("", text: $predicate)
+                FocusedTextField("", text: $predicate)
                     .lineLimit(1)
                     .submitLabel(.done)
                     .font(.system(size: 17))
                     .padding(.trailing, 6)
                     .padding(.vertical, 7)
-                    .focused($isFocused)
+                    .frame(height: 36)
             }
 
             Spacer()
         }
-        .task {
-            isFocused = true
-        }
         .background(Color(red: 0.46, green: 0.46, blue: 0.5, opacity: 0.12))
         .cornerRadius(10)
+    }
+}
+
+private struct FocusedTextField: UIViewRepresentable {
+    @Binding var text: String
+
+    init(_ titleKey: LocalizedStringKey, text: Binding<String>) {
+        _text = text
+    }
+
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField()
+        textField.delegate = context.coordinator
+        textField.returnKeyType = .done
+        textField.becomeFirstResponder()
+        return textField
+    }
+
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = text
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: FocusedTextField
+
+        init(_ textField: FocusedTextField) {
+            self.parent = textField
+        }
+
+        func textField(
+            _ textField: UITextField,
+            shouldChangeCharactersIn range: NSRange,
+            replacementString string: String
+        ) -> Bool {
+            if let currentValue = textField.text as NSString? {
+                let proposedValue = currentValue
+                    .replacingCharacters(in: range, with: string)
+                self.parent.text = proposedValue
+            }
+            return true
+        }
+
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+            return true
+        }
     }
 }
