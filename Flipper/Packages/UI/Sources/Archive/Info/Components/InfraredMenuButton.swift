b4a275c104d625f0a4392443703469051eb8464c
Tony Freeman <tonyfreeman@users.noreply.github.com>
2024-01-16 09:01:29 +0300

    [Archive] Fix infrared menu offset

--- Flipper/Packages/UI/Sources/Archive/Info/Components/InfraredMenuButton.swift
+++ Flipper/Packages/UI/Sources/Archive/Info/Components/InfraredMenuButton.swift
@@ -4,37 +4,28 @@ struct InfraredMenuButton: View {
     let onShare: () -> Void
     let onDelete: () -> Void
 
-    @State private var showInfraredOption = false
+    @State private var showMenu = false
     @State private var showHowToUse: Bool = false
-    @State private var menuOffset = 0.0
 
     var body: some View {
         EllipsisButton {
-            showInfraredOption = true
-        }
-        .background(GeometryReader {
-            Color.clear.preference(
-                key: OffsetKey.self,
-                value: $0.frame(in: .global).origin.y)
-        })
-        .onPreferenceChange(OffsetKey.self) {
-            menuOffset = $0
+            showMenu = true
         }
         .alert(isPresented: $showHowToUse) {
             InfraredHowToUseDialog(isPresented: $showHowToUse)
         }
-        .popup(isPresented: $showInfraredOption) {
+        .popup(isPresented: $showMenu) {
             HStack {
                 Spacer()
                 InfraredMenu(
-                    isPresented: $showInfraredOption,
+                    isPresented: $showMenu,
                     onShare: onShare,
                     onHowTo: { showHowToUse = true },
                     onDelete: onDelete
                 )
             }
             .padding(.horizontal, 14)
-            .offset(y: menuOffset + platformOffset)
+            .offset(y: 44)
         }
     }
 }
