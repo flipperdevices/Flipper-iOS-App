085f863a80bcdf73a0b74222d9efea3e56cccfda
Tony Freeman <tonyfreeman@users.noreply.github.com>
2024-02-02 10:53:50 +0300

    [Apps] Fix outdated alert text

--- Flipper/Packages/UI/Sources/Hub/Apps/Alerts/AppsOutdatedApp.swift
+++ Flipper/Packages/UI/Sources/Hub/Apps/Alerts/AppsOutdatedApp.swift
@@ -23,12 +23,12 @@ struct AppsOutdatedAppAlert: View {
             .padding(.top, 24)
 
             VStack(spacing: 4) {
-                Text("App is Rebuilding")
+                Text("Outdated App")
                     .font(.system(size: 14, weight: .bold))
 
                 Text(
-                    "A new app version is rebuilding on the server. " +
-                    "Please wait, it can take some time."
+                    "Contact the developer on GitHub to request " +
+                    "further app support"
                 )
                 .font(.system(size: 14, weight: .medium))
                 .multilineTextAlignment(.center)
@@ -43,7 +43,7 @@ struct AppsOutdatedAppAlert: View {
                 HStack(spacing: 8) {
                     Image("GitHubButton")
 
-                    Text("Got It")
+                    Text("Go To GitHub")
                         .underline()
                         .font(.system(size: 14, weight: .bold))
                         .foregroundColor(.white)
