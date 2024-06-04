8c9f69d1b6b46c50fdfac0c740b8f6e86dcc5339
Tony Freeman <tonyfreeman@users.noreply.github.com>
2024-06-04 20:50:30 +0300

    [Archive] Enclose some views

--- Flipper/Packages/UI/Sources/Archive/Category/CategoryView.swift
+++ Flipper/Packages/UI/Sources/Archive/Category/CategoryView.swift
@@ -1,42 +1,44 @@
 import Core
 import SwiftUI
 
-struct CategoryView: View {
-    @EnvironmentObject var archive: ArchiveModel
-    @Environment(\.path) private var path
-    @Environment(\.dismiss) private var dismiss
+extension ArchiveView {
+    struct CategoryView: View {
+        @EnvironmentObject var archive: ArchiveModel
+        @Environment(\.path) private var path
+        @Environment(\.dismiss) private var dismiss
 
-    let kind: ArchiveItem.Kind
+        let kind: ArchiveItem.Kind
 
-    var items: [ArchiveItem] {
-        archive.items.filter { $0.kind == kind }
-    }
-
-    var body: some View {
-        ZStack {
-            Text("You have no keys yet")
-                .font(.system(size: 16, weight: .medium))
-                .foregroundColor(.black40)
-                .opacity(items.isEmpty ? 1 : 0)
-
-            ScrollView {
-                CategoryList(items: items) { item in
-                    path.append(ArchiveView.Destination.info(item))
-                }
-                .padding(14)
-            }
+        var items: [ArchiveItem] {
+            archive.items.filter { $0.kind == kind }
         }
-        .background(Color.background)
-        .navigationBarBackground(Color.a1)
-        .navigationBarBackButtonHidden(true)
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            LeadingToolbarItems {
-                BackButton {
-                    dismiss()
+
+        var body: some View {
+            ZStack {
+                Text("You have no keys yet")
+                    .font(.system(size: 16, weight: .medium))
+                    .foregroundColor(.black40)
+                    .opacity(items.isEmpty ? 1 : 0)
+
+                ScrollView {
+                    CategoryList(items: items) { item in
+                        path.append(Destination.info(item))
+                    }
+                    .padding(14)
                 }
-                Text(kind.name)
-                    .font(.system(size: 20, weight: .bold))
+            }
+            .background(Color.background)
+            .navigationBarBackground(Color.a1)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                LeadingToolbarItems {
+                    BackButton {
+                        dismiss()
+                    }
+                    Text(kind.name)
+                        .font(.system(size: 20, weight: .bold))
+                }
             }
         }
     }
--- Flipper/Packages/UI/Sources/Archive/Components/CategoryCard.swift
+++ Flipper/Packages/UI/Sources/Archive/Components/CategoryCard.swift
@@ -2,86 +2,88 @@ import Core
 import SwiftUI
 import OrderedCollections
 
-struct CategoryCard: View {
-    let groups: OrderedDictionary<ArchiveItem.Kind, Int>
-    let deletedCount: Int
+extension ArchiveView {
+    struct CategoryCard: View {
+        let groups: OrderedDictionary<ArchiveItem.Kind, Int>
+        let deletedCount: Int
 
-    var body: some View {
-        VStack(spacing: 0) {
-            ForEach(groups.keys, id: \.self) { key in
-                CategoryLink(
-                    kind: key,
-                    count: groups[key] ?? 0)
-            }
-
-            Divider()
-                .padding(.top, 2)
-                .padding(.bottom, 1)
-
-            CategoryDeletedLink(
-                count: deletedCount)
-        }
-        .background(Color.groupedBackground)
-        .cornerRadius(10)
-        .shadow(color: .shadow, radius: 16, x: 0, y: 4)
-    }
-}
-
-struct CategoryLink: View {
-    let kind: ArchiveItem.Kind
-    let count: Int
-
-    var body: some View {
-        NavigationLink(value: ArchiveView.Destination.category(kind)) {
-            CategoryRow(
-                image: kind.icon,
-                name: kind.name,
-                count: count)
-        }
-    }
-}
-
-struct CategoryDeletedLink: View {
-    let count: Int
-
-    var body: some View {
-        NavigationLink(value: ArchiveView.Destination.categoryDeleted) {
-            CategoryRow(image: nil, name: "Deleted", count: count)
-        }
-    }
-}
-
-struct CategoryRow: View {
-    let image: Image?
-    let name: String
-    let count: Int
-
-    var body: some View {
-        HStack {
-            HStack(spacing: 8) {
-                if let image = image {
-                    image
-                        .resizable()
-                        .renderingMode(.template)
-                        .foregroundColor(.primary)
-                        .frame(width: 24, height: 24)
+        var body: some View {
+            VStack(spacing: 0) {
+                ForEach(groups.keys, id: \.self) { key in
+                    CategoryLink(
+                        kind: key,
+                        count: groups[key] ?? 0)
                 }
-                Text(name)
-                    .font(.system(size: 14, weight: .medium))
-                    .foregroundColor(.primary)
+
+                Divider()
+                    .padding(.top, 2)
+                    .padding(.bottom, 1)
+
+                CategoryDeletedLink(
+                    count: deletedCount)
             }
+            .background(Color.groupedBackground)
+            .cornerRadius(10)
+            .shadow(color: .shadow, radius: 16, x: 0, y: 4)
+        }
+    }
 
-            Spacer()
+    struct CategoryLink: View {
+        let kind: ArchiveItem.Kind
+        let count: Int
 
-            HStack(spacing: 2) {
-                Text(count == 0 ? "" : "\(count)")
-                    .font(.system(size: 14, weight: .medium))
-                    .foregroundColor(.black30)
-                Image("ChevronRight")
+        var body: some View {
+            NavigationLink(value: Destination.category(kind)) {
+                CategoryRow(
+                    image: kind.icon,
+                    name: kind.name,
+                    count: count)
             }
         }
-        .frame(height: 44, alignment: .center)
-        .padding(.leading, 12)
-        .padding(.trailing, 9)
+    }
+
+    struct CategoryDeletedLink: View {
+        let count: Int
+
+        var body: some View {
+            NavigationLink(value: Destination.categoryDeleted) {
+                CategoryRow(image: nil, name: "Deleted", count: count)
+            }
+        }
+    }
+
+    struct CategoryRow: View {
+        let image: Image?
+        let name: String
+        let count: Int
+
+        var body: some View {
+            HStack {
+                HStack(spacing: 8) {
+                    if let image = image {
+                        image
+                            .resizable()
+                            .renderingMode(.template)
+                            .foregroundColor(.primary)
+                            .frame(width: 24, height: 24)
+                    }
+                    Text(name)
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(.primary)
+                }
+
+                Spacer()
+
+                HStack(spacing: 2) {
+                    Text(count == 0 ? "" : "\(count)")
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(.black30)
+                    Image("ChevronRight")
+                }
+            }
+            .frame(height: 44, alignment: .center)
+            .padding(.leading, 12)
+            .padding(.trailing, 9)
+        }
     }
 }
