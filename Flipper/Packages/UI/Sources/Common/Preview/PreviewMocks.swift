f272b37148e9bb14c5777d98b4887edfca50267c
Programistich <dzhos.oleksii@gmail.com>
2024-07-29 13:27:05 +0300

    Lint

--- Flipper/Packages/UI/Sources/Common/Preview/PreviewMocks.swift
+++ Flipper/Packages/UI/Sources/Common/Preview/PreviewMocks.swift
@@ -3,21 +3,16 @@ import Foundation
 extension URL {
     // MARK: Apps
     private static let catalog = "https://catalog.flipperzero.one/api/v0/"
+    private static let path = "application/version/assets/"
 
     static var mockValidAppScreenshotFirst: URL {
-        .init(
-            string: catalog + "application/version/assets/6655c738177869915682353e"
-        )!
+        .init(string: catalog + path + "6655c738177869915682353e")!
     }
     static var mockValidAppScreenshotSecond: URL {
-        .init(
-            string: catalog + "application/version/assets/6655c7381778699156823540"
-        )!
+        .init(string: catalog + path + "6655c7381778699156823540")!
     }
     static var mockValidAppScreenshotThird: URL {
-        .init(
-            string: catalog + "application/version/assets/6655c7381778699156823542"
-        )!
+        .init(string: catalog + path + "6655c7381778699156823542")!
     }
     static var mockUnknownAppScreenshot: URL {
         .init(string: "https://420.com")!
