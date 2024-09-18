d7871b00f6f2c49fe384eba1058267756ed252db
Tony Freeman <tonyfreeman@users.noreply.github.com>
2024-09-18 16:12:01 +0300

    Fix overlay window

--- Flipper/Packages/UI/Sources/InAppNotifications/Overlay/OverlayWindow.swift
+++ Flipper/Packages/UI/Sources/InAppNotifications/Overlay/OverlayWindow.swift
@@ -12,12 +12,75 @@ class OverlayWindow: UIWindow {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        guard let view = super.hitTest(point, with: event) else { return nil }
-        if #available(iOS 18.0, *) {
-            return view
+    // NOTE: Solution from philip_trauner
+    // https://forums.developer.apple.com
+    // /forums/thread/762292?answerId=803885022#803885022
+    private static func _hitTest(
+        _ point: CGPoint,
+        with event: UIEvent?,
+        view: UIView,
+        depth: Int = 0
+    ) -> (view: UIView, depth: Int)? {
+        var deepest: (view: UIView, depth: Int)?
+
+        for subview in view.subviews.reversed() {
+            let converted = view.convert(point, to: subview)
+
+            guard subview.isUserInteractionEnabled,
+                  !subview.isHidden,
+                  subview.alpha > 0,
+                  subview.point(inside: converted, with: event)
+            else {
+                continue
+            }
+
+            let result = if let hit = Self._hitTest(
+                converted,
+                with: event,
+                view: subview,
+                depth: depth + 1
+            ) {
+                hit
+            } else  {
+                (view: subview, depth: depth)
+            }
+
+            if case .none = deepest {
+                deepest = result
+            } else if let current = deepest, result.depth > current.depth {
+                deepest = result
+            }
+        }
+
+        return deepest
+    }
+
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        if #available(iOS 18, *) {
+            guard let view = rootViewController?.view else {
+                return false
+            }
+
+            let hit = Self._hitTest(
+                point,
+                with: event,
+                view: subviews.count > 1 ? self : view
+            )
+
+            return hit != nil
         } else {
-            return rootViewController?.view == view ? nil : view
+            return super.point(inside: point, with: event)
+        }
+    }
+
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        if #available(iOS 18, *) {
+            return super.hitTest(point, with: event)
+        } else {
+            guard let hit = super.hitTest(point, with: event) else {
+                return .none
+            }
+            return rootViewController?.view == hit ? .none : hit
         }
     }
 }
